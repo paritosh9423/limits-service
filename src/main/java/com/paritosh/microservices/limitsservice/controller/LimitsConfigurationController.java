1f02bf3c2a836506d142def30e278ac56a198566
ppradeep <paritosh.9423@gmail.com>
2020-01-27 22:13:55 +0530

    added simple configuration file using @ConfigurationProperties and read value from application.properites file

--- src/main/java/com/paritosh/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ src/main/java/com/paritosh/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -1,14 +1,18 @@
 package com.paritosh.microservices.limitsservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.paritosh.microservices.limitsservice.beans.Configuration;
 import com.paritosh.microservices.limitsservice.beans.LimitConfigurtion;
 
 @RestController
 public class LimitsConfigurationController {
+	@Autowired
+	private Configuration config;
 	@GetMapping("/limits")
 	public LimitConfigurtion retrieveLimitsFromConfiguration() {
-		return new LimitConfigurtion(1000, 1);
+		return new LimitConfigurtion(config.getMaximum(), config.getMinimum());
 	}
 }
