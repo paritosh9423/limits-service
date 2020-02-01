4c8d64c07e5d16546485c8dc3123267fb511bbf9
ppradeep <paritosh.9423@gmail.com>
2020-02-01 16:39:01 +0530

    added support of Hysterix for fault tolerance

--- src/main/java/com/paritosh/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ src/main/java/com/paritosh/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.paritosh.microservices.limitsservice.beans.Configuration;
 import com.paritosh.microservices.limitsservice.beans.LimitConfigurtion;
 
@@ -14,5 +15,18 @@ public class LimitsConfigurationController {
 	@GetMapping("/limits")
 	public LimitConfigurtion retrieveLimitsFromConfiguration() {
 		return new LimitConfigurtion(config.getMaximum(), config.getMinimum());
+	}
+	
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+	public LimitConfigurtion retrieveConfiguration() {
+		throw new RuntimeException("Not Available");
+	}
+	
+	public LimitConfigurtion fallbackRetrieveConfiguration() {
+		//return default config in case of any exceptions
+		return new LimitConfigurtion(9,999);
+	
+	
 	}
 }
